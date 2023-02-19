6dad3c18c89a2dd4b5a8b49f3c5cdca63fff4218
Vince Smykal <you@example.com>
2023-02-18 22:59:53 -0500

    adding assignment 6

---
+++ assignment 6/js/fnText.js
@@ -0,0 +1,22 @@
+function enlargeText(){
+    document.getElementById("p1").style.fontSize = "5em";
+    document.getElementById("p2").style.fontSize = "7em";
+    document.getElementById("p3").style.fontSize = "9em";
+    document.getElementById("p4").style.fontSize = "11em";
+    document.getElementById("p5").style.fontSize = "13em";
+}
+function decreaseText(){
+    document.getElementById("p1").style.fontSize = "1em";
+    document.getElementById("p2").style.fontSize = "2em";
+    document.getElementById("p3").style.fontSize = "3em";
+    document.getElementById("p4").style.fontSize = "4em";
+    document.getElementById("p5").style.fontSize = "5em";
+}
+function enlargeHeading(){
+    document.getElementById("h1").style.fontSize = "5em";
+    document.getElementById("h2").style.fontSize = "7em";
+}
+function decreaseHeading(){
+    document.getElementById("h1").style.fontSize = "1em";
+    document.getElementById("h2").style.fontSize = "2em";
+}
---
+++ assignment 6/js/hideImg.js
@@ -0,0 +1,6 @@
+//function hideImg.js
+//Description: Javascript to hide images
+
+function hideIt(x){
+    document.getElementById("color"+x).style.visibility='hidden';
+}
